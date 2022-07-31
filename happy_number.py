eaf4f1368a981d1f29fc693e7440872ddcb49959
o0PHOENIX0o <100501442+o0PHOENIX0o@users.noreply.github.com>
2022-07-31 20:35:47 +0530

    Add files via upload

---
+++ happy_number.py
@@ -0,0 +1,25 @@
+def Happy_number(num):
+    sum=0
+    while num>0:
+        rem=num%10
+        sum = sum + rem*rem
+        num=num//10
+    if sum!=1 and sum!=4:
+        return Happy_number(sum)
+    else:
+        return sum
+while True:
+    number = input('enter a number ')
+    if number.lower() in ['exit','done','quit']:
+        print('\n\t\tprograme terminated\n')
+        break
+    else:
+        try:
+            number=int(number)
+            ans=Happy_number(number)
+            if ans == 1:
+                print(f'\n\t\t{number} is a happy number\n')
+            else:
+                print(f'\n\t\t{number} is not a happy number\n')
+        except Exception as e:
+            print('\n\t\t\tinvalid input\n')
